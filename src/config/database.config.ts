7ef36722bd9347d4dc1316de6bb3ad4c1bf9853e
Kashif Rezwi <kashifrezwi850@gmail.com>
2025-10-11 03:51:04 +0530

    Update database configuration to disable table auto-creation in production

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -10,8 +10,7 @@ export default registerAs(
         type: 'postgres',
         url: process.env.DATABASE_URL,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true, // Auto-creates tables (for very first time)
-        // synchronize: false, // NEVER true in production! (can drop data)
+        synchronize: false, // NEVER true in production! (can drop data)
         ssl: {
           rejectUnauthorized: false, // Required for Render Postgres
         },
