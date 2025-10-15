abcc6cf8465f6e690abbc139c43ce48966697f33
Kashif Rezwi <kashifrezwi850@gmail.com>
2025-10-16 00:48:36 +0530

    Remove optional conversationId field from ChatRequestDto in chat module

--- src/modules/chat/dto/chat-request.dto.ts
+++ src/modules/chat/dto/chat-request.dto.ts
@@ -3,8 +3,4 @@ import { IsArray, IsString, IsOptional } from 'class-validator';
 export class ChatRequestDto {
   @IsArray()
   messages: any[];
-
-  @IsOptional()
-  @IsString()
-  conversationId?: string;
 }
